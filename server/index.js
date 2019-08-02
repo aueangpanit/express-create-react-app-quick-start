4a22268bb83ceb8e4ec48da335a73d528b9f755d
Aue Angpanitcharoen <Aue Angpanitcharoen>
2019-08-02 22:32:48 +0100

    added mongodb

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/xeonoex-bot', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected with db');
+});
+
 const app = express();
 require('./routes/flightRoutes')(app);
 
